1643b4cdd863f936ee6b1e106b9bb844014d4f3e
xtombrock <xtombrock1989@gmail.com>
2023-04-19 02:11:32 +0100

    working hello world

--- client/src/index.js
+++ client/src/index.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+// Context
+import UserContextProvider from './context/UserContext';
+import ToggleContextProvider from './context/ToggleContext';
+// Styles
+import './styles/index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+    <UserContextProvider>
+      <ToggleContextProvider>
+        <App />
+      </ToggleContextProvider>
+    </UserContextProvider>
+  </BrowserRouter>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
