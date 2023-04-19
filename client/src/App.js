faf173a59212e2d6abd509785b933545ff6fe407
xtombrock <xtombrock1989@gmail.com>
2023-04-19 02:18:30 +0100

    routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,3 +1,4 @@
+import { Routes, Route } from 'react-router-dom';
 // Analytics
 import ReactGA from 'react-ga';
 
@@ -10,7 +11,57 @@ function App() {
   ReactGA.pageview('/login');
   ReactGA.pageview('/store');
   ReactGA.pageview('/new-project');
-  return <>Hello World</>;
+  return (
+    <>Hello</>
+    // <Routes>
+    //   <Route path='/' index element={<IndexPage />} />
+    //   <Route path='/contact' element={<Contact />} />
+
+    //   {/* Secure routes */}
+    //   <Route
+    //     path='/admin'
+    //     element={
+    //       <AuthenticateAdmin>
+    //         <AdminPanel />
+    //       </AuthenticateAdmin>
+    //     }
+    //   />
+    //   <Route
+    //     path='/development'
+    //     element={
+    //       <AuthenticateDeveloper>
+    //         <DeveloperPanel />
+    //       </AuthenticateDeveloper>
+    //     }
+    //   />
+
+    //   {/* User Routes */}
+    //   <Route path='/login' element={<Login />} />
+    //   <Route path='/register' element={<Register />} />
+    //   <Route path='/reset-lost-password' element={<ResetPassword />} />
+
+    //   <Route
+    //     path='/account'
+    //     element={
+    //       <AuthenticateUser>
+    //         <Account />
+    //       </AuthenticateUser>
+    //     }
+    //   />
+
+    //   {/* User data */}
+    //   <Route path='users/verify/:userId/:uniqueString' element={<Verify />} />
+
+    //   <Route
+    //     path='users/:userId/update-password'
+    //     element={<UpdatePassword />}
+    //   />
+    //   <Route
+    //     path='users/reset-lost-password/:userId/:uniqueString'
+    //     element={<EnterNewPassword />}
+    //   />
+    // </Routes>
+  );
 }
 
 export default App;
