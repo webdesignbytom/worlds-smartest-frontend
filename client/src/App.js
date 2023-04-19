15db0b1736faf880e70fe007bb0c41e8cbb80fcb
xtombrock <xtombrock1989@gmail.com>
2023-04-19 02:15:10 +0100

    404

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,15 @@
+// Analytics
+import ReactGA from 'react-ga';
+
 function App() {
+  ReactGA.initialize('G-2DWCKVD9WK');
+  ReactGA.pageview('/');
+  ReactGA.pageview('/contact');
+  ReactGA.pageview('/design');
+  ReactGA.pageview('/portfolio');
+  ReactGA.pageview('/login');
+  ReactGA.pageview('/store');
+  ReactGA.pageview('/new-project');
   return <>Hello World</>;
 }
 
