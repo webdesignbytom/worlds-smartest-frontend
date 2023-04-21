79359519136f76483a7ccd6beb4b5585f346f761
xtombrock <xtombrock1989@gmail.com>
2023-04-21 10:11:33 +0100

    ave

--- client/src/components/reports/BugReport.jsx
+++ client/src/components/reports/BugReport.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from 'react';
+// Icons
+import { AiFillBug } from 'react-icons/ai';
 
 function BugReport() {
+
+    const writeBugReport = () => {
+        console.log('bug')
+    }
+
   return (
-    <div>BugReport</div>
-  )
+    <section onClick={writeBugReport} className='absolute no__highlights grid right-4 top-4 cursor-pointer'>
+      <article className='flex justify-end items-center bg-gray-200 rounded-xl'>
+        <span className='mx-2'>
+          <p>BUG REPORT</p>
+        </span>
+        <AiFillBug size={40} />
+      </article>
+    </section>
+  );
 }
 
-export default BugReport
+export default BugReport;
--- client/src/pages/beta/BetaTestQuestions.jsx
+++ client/src/pages/beta/BetaTestQuestions.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
+import BugReport from '../../components/reports/BugReport';
 
 function BetaTestQuestions() {
   return (
-    <div className='bg-red-300 min-h-screen h-screen lg:overflow-hidden grid '>
-      BetaTestQuestions
+    <div className='relative test__bg bg-red-300 min-h-screen h-screen lg:overflow-hidden grid p-2'>
+      <main className='bg-white grid justify-center items-center'>
+        <BugReport />
+        <section className='absolute no__highlights grid left-4 top-4 group'>
+          <div className='flex justify-end items-center'>
+            <p className='font-bold'>BETA TEST MODE</p>
+          </div>
+        </section>
+        <section>BetaTestQuestions</section>
+      </main>
     </div>
   );
 }
